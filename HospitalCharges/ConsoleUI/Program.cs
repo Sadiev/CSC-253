033d6b973de75226c77425ac3ef78371a28d7289
Shod <dilshod.sadiev@outlook.com>
2019-08-24 21:43:02 -0400

    Hospital Charges

---
+++ HospitalCharges/ConsoleUI/Program.cs
@@ -0,0 +1,53 @@
+﻿/**
+* 08/24/2019
+* CSC 253
+* Dilshod Sadiev
+* Hospital Charges
+*/
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        const decimal COST= 350;
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"\nTotal charges is ${CalcTotalCharges(CalcStayCharges(), CalcMiscCharges())}");
+        }
+
+        private static decimal CalcTotalCharges(decimal stayCharges, decimal miscCharges)
+        {
+            return stayCharges + miscCharges;
+        }
+
+        static decimal CalcStayCharges()
+        {
+            ushort days = 0;
+            Console.Write("Enter the number of days spent in the hospital > ");
+            while (!ushort.TryParse(Console.ReadLine(), out days))
+            {
+                Console.Write("Not a valid number. Please try again > ");
+            }
+            return days*COST;
+        }
+
+        static decimal CalcMiscCharges()
+        {
+            return InputAmount("Enter the amount of medication charges > ")+
+                   InputAmount("Enter the amount of surgical charges > ")+
+                   InputAmount("Enter the amount of lab fees > ")+
+                   InputAmount("Enter the amount of physical rehabilitation charges > ");
+        }
+        static decimal InputAmount(string txt)
+        {
+            decimal amount = 0;
+            Console.Write(txt);
+            while (!decimal.TryParse(Console.ReadLine(), out amount))
+            {
+                Console.Write("Not a valid amount. Please try again > ");
+            }
+            return amount;
+        }
+    }
+}
