f8438c14b667f9b09f80cef4fb31b1e4b1242b11
Shod <dilshod.sadiev@outlook.com>
2019-09-18 08:33:45 -0400

    test

---
+++ WriteClassInformationFile/WriteFileLibrary/WriteFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteFileLibrary
+{
+    public class WriteFile
+    {
+        public static void CreateFile()
+        {
+            try
+            {
+                StreamWriter outputFile;
+                outputFile.WriteLine();
+                outputFile.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+        
+
+    }
+}
