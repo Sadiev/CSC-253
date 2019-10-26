becbd1561eb28e72312a6a94d36dce6484d82a8f
Shod <dilshod.sadiev@outlook.com>
2019-10-25 20:08:40 -0400

    ShiftSupervisor Class

---
+++ EmployeeProductionWorker/Classes/ShiftSupervisor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class ShiftSupervisor : Employee
+    {
+        public ShiftSupervisor(decimal annualSalary, decimal annualBonus, string employeeName, int employeeNumber) : base(employeeName, employeeNumber)
+        {
+            AnnualSalary = annualSalary;
+            AnnualBonus = annualBonus;           
+        }
+        public decimal AnnualSalary { get; set; }
+        public decimal AnnualBonus { get; set; }
+    }
+}
--- EmployeeProductionWorker/EmployeeProductionWorker/Program.cs
+++ EmployeeProductionWorker/EmployeeProductionWorker/Program.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 using Classes;
 
 /**
-* 10/23/2019
+* 10/25/2019
 * CSC 253
 * Dilshod Sadiev
-* Employee and ProductionWorker Classes
+* ShiftSupervisor Class / Employee and ProductionWorker Classes
 */
 
 namespace EmployeeProductionWorker
@@ -18,14 +18,52 @@ namespace EmployeeProductionWorker
     {
         static void Main(string[] args)
         {
+            //NewProductionWorker();
+            NewShiftSupervisor();
+
+            Console.WriteLine("\nPress any key to exit ...");
+            Console.ReadLine();
+        }
+
+        private static void NewShiftSupervisor()
+        {
             string employeeName;
-            int employeeNumber=0;
+            int employeeNumber = 0;
+            decimal annual = 0;
+            decimal bonus=0;
+
+            Console.Write("Enter an employee name > ");
+            employeeName = Console.ReadLine();
+
+            Console.Write("Enter an employee number > ");
+            while (!int.TryParse(Console.ReadLine(), out employeeNumber))
+            {
+                Console.Write("Enter a valid number > ");
+            }
+            Console.Write("Enter an annual salary > ");
+            while (!decimal.TryParse(Console.ReadLine(), out annual))
+            {
+                Console.Write("Not a valid salary. Try again > ");
+            }
+            Console.Write("Enter an annual bonus > ");
+            while (!decimal.TryParse(Console.ReadLine(), out bonus))
+            {
+                Console.Write("Not a valid bonus. Try again > ");
+            }
+            ShiftSupervisor supervisor = new ShiftSupervisor(annual, bonus, employeeName, employeeNumber);
+            Console.WriteLine($"\nName: {supervisor.EmployeeName}\nEmployee number: {supervisor.EmployeeNumber}\nAnnual salary: {supervisor.AnnualSalary}\nAnnual bonus: {supervisor.AnnualBonus}");
+        }
+
+        private static void NewProductionWorker()
+        {
+            string employeeName;
+            int employeeNumber = 0;
             int shift = 0;
             decimal hourlyPayRate;
 
             Console.Write("Enter an employee name > ");
             employeeName = Console.ReadLine();
-            
+
             Console.Write("Enter an employee number > ");
             while (!int.TryParse(Console.ReadLine(), out employeeNumber))
             {
@@ -48,10 +86,6 @@ namespace EmployeeProductionWorker
 
             Console.WriteLine($"\nEmployee name: {epw.EmployeeName}\nEmployee number: {epw.EmployeeNumber}\n" +
                               $"Shift: {epw.Shift}\nHourly pay rate: ${epw.HourlyPayRate}");
-            
-            Console.WriteLine("\nPress any key to exit ...");
-            Console.ReadLine();
-
         }
     }
 }
