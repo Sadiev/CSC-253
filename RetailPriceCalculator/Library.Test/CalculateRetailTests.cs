26eaa108251383a2004d66b9c15a96cfe4b77432
Shod <dilshod.sadiev@outlook.com>
2019-11-08 17:53:38 -0500

    Unit Testing

---
+++ RetailPriceCalculator/Library.Test/CalculateRetailTests.cs
@@ -0,0 +1,24 @@
+﻿using RetailPriceCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Library.Test
+{
+    public class CalculateRetailTests
+    {
+        [Fact]
+        public void CalculateRetail()
+        {
+            //Arrange
+            decimal expected = 10;
+            //Act
+            decimal actual = 4;//Calculate.CalculateRetail(5, 100);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ RetailPriceCalculator/RetailPriceCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal wholesaleCost = 0;
+            byte percentage = 0;
+            Console.Write("Enter an item’s wholesale cost > ");
+            while (!decimal.TryParse(Console.ReadLine(),out wholesaleCost))
+            {
+                Console.Write("Not a valid price, please try again > ");
+            }
+            Console.Write("\nEnter markup percentage > ");
+            while (!byte.TryParse(Console.ReadLine(), out percentage))
+            {
+                Console.Write("Not a valid percentage, please try again > ");
+            }
+            Console.WriteLine($"\nThe item’s wholesale cost is ${wholesaleCost} and its markup percentage is {percentage} percent\n" +
+                              $"The item’s retail price is ${Calculate.CalculateRetail(wholesaleCost, percentage)}");
+            Console.WriteLine("\nPress any key to exit ...");
+            Console.ReadKey();
+        }
+        
+    }
+    public class Calculate
+    {
+        static public decimal CalculateRetail(decimal wholesaleCost, byte percentage)
+        {
+            return (wholesaleCost * percentage / 100) + wholesaleCost;
+        }
+    }
+}
