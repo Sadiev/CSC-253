8218579a99c965c56dc3fabfef6256821c236f73
Shod <dilshod.sadiev@outlook.com>
2019-11-13 19:06:44 -0500

    WinForm

---
+++ EmployeeClass/WindowsForms/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsForms
+{
+    partial class EmployeeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EmployeeListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // EmployeeListBox
+            // 
+            this.EmployeeListBox.BackColor = System.Drawing.SystemColors.MenuText;
+            this.EmployeeListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EmployeeListBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeListBox.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.EmployeeListBox.FormattingEnabled = true;
+            this.EmployeeListBox.ItemHeight = 23;
+            this.EmployeeListBox.Location = new System.Drawing.Point(0, 0);
+            this.EmployeeListBox.Name = "EmployeeListBox";
+            this.EmployeeListBox.Size = new System.Drawing.Size(845, 450);
+            this.EmployeeListBox.TabIndex = 0;
+            // 
+            // EmployeeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 450);
+            this.Controls.Add(this.EmployeeListBox);
+            this.Name = "EmployeeForm";
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.EmployeeForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox EmployeeListBox;
+    }
+}
+
---
+++ EmployeeClass/WindowsForms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using HRLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class EmployeeForm : Form
+    {
+        public EmployeeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EmployeeForm_Load(object sender, EventArgs e)
+        {
+            Employee emp1 = new Employee("Susan Meyers", 47899, "Accounting", "Vice President");
+
+            Employee emp2 = new Employee("Mark Jones", 39119);
+            emp2.Department = "IT";
+            emp2.Position = "Programmer";
+
+            Employee emp3 = new Employee();
+            emp3.Name = "Joy Rogers";
+            emp3.IdNumber = 81774;
+            emp3.Department = "Manufacturing";
+            emp3.Position = "Engineer";
+            EmployeeListBox.Items.Add($"{"Name",-15}{"ID Number",-15}{"Department",-15}{"Position"}");
+
+            EmployeeListBox.Items.Add(("").PadRight(60, '-'));
+            EmployeeListBox.Items.Add($"{emp1.Name,-15}{emp1.IdNumber,-15}{emp1.Department,-15}{emp1.Position}");
+            EmployeeListBox.Items.Add($"{emp2.Name,-15}{emp2.IdNumber,-15}{emp2.Department,-15}{emp2.Position}");
+            EmployeeListBox.Items.Add($"{emp3.Name,-15}{emp3.IdNumber,-15}{emp3.Department,-15}{emp3.Position}");
+
+        }
+    }
+}
